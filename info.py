8e29c799c7f522813bf26a9d4d6ecbb41633f997
nani3211 <pakkavillagefun@gmail.com>
2025-10-08 09:06:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://southern-colleen-gangadhar3211-176b26b2.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
